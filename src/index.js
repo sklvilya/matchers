0d9ef37733d4d50f5566948df93525ee8f5dbdcb
sklvilya <sklvilyaomsk@gmail.com>
2023-02-24 12:15:40 +0600

    fixed index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 /* eslint linebreak-style: ["error", "windows"]  */
 export const heroNotSort = [
-  { name: 'мечник', health: 10 },
   { name: 'маг', health: 100 },
   { name: 'лучник', health: 80 },
+  { name: 'мечник', health: 10 },
 ];
 export default function toSortHero(hero) {
   return hero.sort((a, b) => b.health - a.health);
