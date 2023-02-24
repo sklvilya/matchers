b31a61481537f7cd0a8298a8472b55c5b4bc014a
sklvilya <sklvilyaomsk@gmail.com>
2023-02-24 12:32:25 +0600

    eslintrc linebreak-style

--- src/index.js
+++ src/index.js
@@ -1,9 +1,7 @@
-/* eslint linebreak-style: ["error", "windows"]  */
 export const heroNotSort = [
   { name: 'маг', health: 100 },
   { name: 'лучник', health: 80 },
   { name: 'мечник', health: 10 },
-];
-export default function toSortHero(hero) {
+]; export default function toSortHero(hero) {
   return hero.sort((a, b) => b.health - a.health);
 }
--- src/test/test.js
+++ src/test/test.js
@@ -1,4 +1,3 @@
-/* eslint linebreak-style: ["error", "windows"] */
 import toSortHero, { heroNotSort } from '../index';
 
 test('check sort heroes', () => {
